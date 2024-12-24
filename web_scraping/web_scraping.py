15bb12f86d2e125c06166b83e599ba902140939c
tajulkhan <tajulbasheer@gmail.com>
2024-12-24 12:53:07 +0530

    web scraping practice

--- web_scraping/web_scraping.py
+++ web_scraping/web_scraping.py
@@ -1,5 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://wandertaj.netlify.app/"
+
 page= requests.get(URL)
-print(page.text)
+soup = BeautifulSoup(page.text, "html.parser")
+res = soup.find(class_="sec-quotes").text
+link = soup.find("a", class_="cv-btn-theme")
+if link:  # Ensure the link exists
+    print(link['href'])
+
+print(res)
+print(soup.title.text)
+# print(page.text)
+
+for data in link:
+    print(data)
