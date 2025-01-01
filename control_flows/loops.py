a758fa17f80c02917956cf7007a756b13097b117
tajulkhan <tajulbasheer@gmail.com>
2025-01-01 12:49:48 +0530

    +2

--- control_flows/loops.py
+++ control_flows/loops.py
@@ -1,3 +1,9 @@
 # Loop through a range of numbers from 1 to 5
 for i in range(1, 6):
     print(f"Iteration {i}")
+
+# While loop 
+count = 1
+while count <= 5:
+    print(f"Iteration {count}")
+    count += 1  
