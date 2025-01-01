6805e3ed3c5a10359e475354a27c0cca95bceb28
tajulkhan <tajulbasheer@gmail.com>
2025-01-01 12:50:46 +0530

    +3

--- control_flows/loops.py
+++ control_flows/loops.py
@@ -7,3 +7,10 @@ count = 1
 while count <= 5:
     print(f"Iteration {count}")
     count += 1  
+
+# Example with break statement
+for i in range(1, 11):
+    if i == 6:
+        print("Breaking the loop at 6")
+        break  # Exit the loop
+    print(i)
